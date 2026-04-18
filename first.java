4c7741ce516f7f0d6d17b648f05a7613f4b4ad4c
Pawan Kumar <pawankr45717@gmail.com>
2026-04-18 11:23:41 +0530

    new features

--- first.java
+++ first.java
@@ -2,5 +2,8 @@ public class first {
    public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Welcome to Java Programming");
+    int a = 2;
+    int b = 3;
+    System.out.println(a + b);
    } 
 }
